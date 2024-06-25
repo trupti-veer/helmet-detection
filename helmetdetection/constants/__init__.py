b9a24482b9ea25f4108c708a17666969b251dc6c
Trupti Veer <jagtap.trupti@gmail.com>
2024-06-25 16:37:07 +0530

    Added data transformation

---
+++ helmetdetection/constants/__init__.py
@@ -0,0 +1,40 @@
+import os
+import torch
+from datetime import datetime
+
+TIMESTAMP: str = datetime.now().strftime("%m_%d_%Y_%H_%M_%S")
+
+# Data Ingestion Constants
+ARTIFACTS_DIR = os.path.join("artifacts", TIMESTAMP)
+BUCKET_NAME = 'test-helmetdetection'
+ZIP_FILE_NAME = 'data.zip'
+ANNOTATIONS_COCO_JSON_FILE = '_annotations.coco.json'
+
+INPUT_SIZE = 416
+HORIZONTAL_FLIP = 0.3
+VERTICAL_FLIP = 0.3
+RANDOM_BRIGHTNESS_CONTRAST = 0.1
+COLOR_JITTER = 0.1
+BBOX_FORMAT = 'coco'
+
+RAW_FILE_NAME = 'helmetdetection'
+
+# Data ingestion constants 
+DATA_INGESTION_ARTIFACTS_DIR = 'DataIngestionArtifacts'
+DATA_INGESTION_TRAIN_DIR = 'train'
+DATA_INGESTION_TEST_DIR = 'test'
+DATA_INGESTION_VALID_DIR = 'valid'
+
+# Data transformation constants 
+DATA_TRANSFORMATION_ARTIFACTS_DIR = 'DataTransformationArtifacts'
+DATA_TRANSFORMATION_TRAIN_DIR = 'Train'
+DATA_TRANSFORMATION_TEST_DIR = 'Test'
+DATA_TRANSFORMATION_TRAIN_FILE_NAME = "train.pkl"
+DATA_TRANSFORMATION_TEST_FILE_NAME = "test.pkl"
+DATA_TRANSFORMATION_TRAIN_SPLIT = 'train'
+DATA_TRANSFORMATION_TEST_SPLIT = 'test'
+
+# AWS CONSTANTS
+AWS_ACCESS_KEY_ID = "AWS_ACCESS_KEY_ID"
+AWS_SECRET_ACCESS_KEY = "AWS_SECRET_ACCESS_KEY"
+AWS_REGION_NAME = "us-east-1"
